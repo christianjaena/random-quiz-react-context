b2eeb6d3f0a0d734f9683ba6eaf18c0023bb9648
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-07 00:19:13 +0800

    Updates

---
+++ precache-manifest.7e60c7bee1feb413302db1f0f251b884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1f3f5d26c0c6a2ec6b9b5194879ea2",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "ea4e55df6a89ec203826",
+    "url": "/react-quiz/static/js/2.79345f8b.chunk.js"
+  },
+  {
+    "revision": "1b0e9eedc9c6ce185e4815b502179b21",
+    "url": "/react-quiz/static/js/2.79345f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190793c67d26cc694c89",
+    "url": "/react-quiz/static/js/main.5cf0dfd3.chunk.js"
+  },
+  {
+    "revision": "3218b795fbcbeab53d41",
+    "url": "/react-quiz/static/js/runtime-main.6523520f.js"
+  }
+]);
